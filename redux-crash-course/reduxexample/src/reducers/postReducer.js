25c0e234d81d2cc30d7ed801b225677a29e1a48f
Michel Araujo <araujo_michel@yahoo.com>
2018-08-07 00:32:56 -0300

    Create postReducer and add reduxDevTools

--- redux-crash-course/reduxexample/src/reducers/postReducer.js
+++ redux-crash-course/reduxexample/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS, NEW_POST } from '../actions/types';
+import { FETCH_POSTS } from '../actions/types';
 
 const initialState = {
   items: [],
@@ -7,6 +7,11 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload
+      }
     default:
       return state;
   }
