425b0cc68ef6dd7ab57e43cfe68e538d8c60e88a
Michel Araujo <araujo_michel@yahoo.com.br>
2020-02-17 19:01:40 -0300

    Create files memo.js

---
+++ memoFix.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const http = require("http");
+
+const filename = "./requests.json";
+
+const readRequests = () => {
+  try {
+    return fs.readFileSync(filename);
+  } catch (e) {
+    return "[]";
+  } 
+};
+
+const writeRequest = req => {
+  const requests = JSON.parse(readRequests());
+  requests.push({
+    url: req.url,
+    date: new Date(),
+  });
+  fs.writeFileSync(filename, JSON.stringify(requests));
+}
+
+const server = http.createServer((req, res) => {
+  writeRequest(req);
+  res.end(readRequests());
+});
+
+server.listen(3000);
+console.log("Server listening to port 3000");
