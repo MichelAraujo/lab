68b0f99f11a3b83d44049afe71aac656c0cfff69
Michel Araujo <araujo_michel@yahoo.com.br>
2020-04-11 15:29:29 -0300

    Add new exemplas of the nodeschool

---
+++ nodeschool/my-scenario-runner.js
@@ -0,0 +1,43 @@
+module.exports = scenario;
+
+var bunyan = require("bunyan");
+var log = bunyan.createLogger({ name: "scenario" });
+
+function scenario(log, cb) {
+  function start() {
+    process.nextTick(thing);
+  }
+
+  var value = 97;
+  log.info({ value: value }, "scenario");
+
+  function foo() {
+    value *= 13;
+    log.info({ value: value }, "foo");
+    cb(value);
+    log.info({ value: value }, "foo");
+  }
+
+  start();
+
+  function racer() {
+    value &= 255;
+    log.info({ value: value }, "racer");
+    setTimeout(foo, 0);
+  }
+
+  log.error('new add');
+  value = 213;
+  log.info({ value: value }, "scenario");
+
+  function thing() {
+    value += 131;
+    log.info({ value: value }, "thing");
+    process.nextTick(racer);
+  }
+}
+
+scenario(log, (value) => {
+  log.info({ value: value }, "cb");
+  console.log(value);
+});
---
+++ nodeschool/scenario.js
@@ -0,0 +1,19 @@
+// scenario.js
+var fs = require("fs");
+
+var peach = function (obj) {
+  console.trace("traced");
+  console.dir(obj);
+};
+
+var bowser = function (callback) {
+  fs.readFile(process.argv[2], {encoding : "utf8"}, callback);
+};
+
+var koopa = function (error, file) {
+  console.error("Test ", error);
+
+  peach(JSON.parse(file));
+};
+
+bowser(koopa);
